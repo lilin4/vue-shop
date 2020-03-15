04397807b42538e403c9be5c6a25a8dcc1243194
17601002613@163.com <linli1993>
2020-03-15 19:12:27 +0800

    登陆页面完成

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  methods: {
+    // 退出按钮
+    logout() {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './plugins/element.js'
+import '@/plugins/element.js'
+/* 导入图标库 */
+import './assets/fonts/iconfont.css'
+// 导入全局样式表
+import './assets/css/global.css'
+
+// 配置axios
+import axios from 'axios'
+// 默认请求路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
